06c0360f7d051d70c4d05e5d5812b07b021a867e
blahfrk <blahfrk@gmail.com>
2024-12-20 17:22:32 +0800

    Implemented the retreiving and displaying of tweets Data

--- twitterX/src/pages/Home.jsx
+++ twitterX/src/pages/Home.jsx
@@ -15,16 +15,28 @@ import {
 import Tweet from "./Tweet";
 import { useNavigate } from "react-router-dom";
 import firebaseApp from "./firebaseConfig";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"; // import firebase "auth" service for user authentication
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  Timestamp,
+  onSnapshot,
+} from "firebase/firestore"; // import firebase "firestore" service for tweets database
 import { useState, useEffect } from "react";
 
 export default function Home() {
   let navigate = useNavigate();
+  const auth = getAuth(firebaseApp); // Initialized connection to firebase "auth" service
+  const db = getFirestore(firebaseApp); // Initialized connection to firebase "firestore" service or basically the database
 
   const [userProfile, setUserProfile] = useState("");
+  const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]); // Collection of tweets
+
+  const [buttonLoading, setButtonLoading] = useState(false);
 
   useEffect(() => {
-    const auth = getAuth(firebaseApp);
     // To check if there is an authenticated/logged in account (if there is a current session of account)
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -39,11 +51,37 @@ export default function Home() {
         navigate("./login");
       }
     });
+
+    // Retreive tweets from firestore
+    onSnapshot(collection(db, "tweets"), (snapshot) => {
+      setTweets(snapshot.docs.map((t) => t.data()));
+    });
   }, []);
+
+  const createTweet = () => {
+    setButtonLoading(true); // Loading icon in tweet button
+    if (tweet !== "") {
+      const tweetData = {
+        // Storing of the user data in the firestore for identification of who tweeted the tweet and the tweet itself
+        body: tweet, // The tweet itself
+        user_email: userProfile.email, // email of the user who tweeted
+        name: userProfile.name, // name of the user who tweeted
+        date_posted: Timestamp.now(), // time & date tweet posted
+      };
+      // Adding the tweet doc to firestore database named as 'tweets'
+      addDoc(collection(db, "tweets"), tweetData).then(() => {
+        setTweet("");
+        setButtonLoading(false);
+      });
+    } else {
+      alert("Tweet cannot be empty!").then(() => {
+        setButtonLoading(false);
+      });
+    }
+  };
 
   // To stop the current session or basically logs out the current authenticated user
   const Logout = () => {
-    const auth = getAuth(firebaseApp);
     signOut(auth).then(() => {
       navigate("./login");
     });
@@ -70,15 +108,37 @@ export default function Home() {
           <Card mt={5} p={5}>
             <FormControl>
               <FormLabel>What's on your mind? 💬</FormLabel>
-              <Input type="email" />
+              <Input
+                disabled={buttonLoading} // Input field disabled if button loading
+                type="text"
+                onChange={(e) => {
+                  setTweet(e.target.value);
+                }}
+                value={tweet}
+              />
             </FormControl>
-            <Button w="100px" colorScheme="blue" mt={3} size="sm">
+            <Button
+              isLoading={buttonLoading} // ChakraUI button loading
+              w="100px"
+              colorScheme="blue"
+              mt={3}
+              size="sm"
+              onClick={createTweet}
+            >
               Tweet
             </Button>
           </Card>
           <Divider my={5}></Divider>
 
-          <Tweet />
+          {tweets.map((tweetRecord) => (
+            <Tweet
+              key={tweetRecord.id}
+              body={tweetRecord.body}
+              email={tweetRecord.user_email}
+              name={tweetRecord.name}
+              date_posted={tweetRecord.date_posted.toDate().toString()}
+            ></Tweet>
+          ))}
         </Box>
       </Flex>
     </Container>
--- twitterX/src/pages/Tweet.jsx
+++ twitterX/src/pages/Tweet.jsx
@@ -1,14 +1,14 @@
 import { Card, Text, Divider } from "@chakra-ui/react";
 
-export default function Tweet() {
+export default function Tweet({ body, email, name, date_posted }) {
   return (
     <Card mt={5} px={5} pb={5} pt={2}>
-      <Text fontWeight="bold">John Doe</Text>
+      <Text fontWeight="bold">{name}</Text>
       <Text fontSize="xs" color="gray">
-        🕒 a few minites ago
+        🕒 {date_posted}
       </Text>
       <Divider my={2} color="lightgray"></Divider>
-      <Text>The quick brown fox jumped over the lazy dog.</Text>
+      <Text>{body}</Text>
     </Card>
   );
 }
